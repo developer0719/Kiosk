ac467357ad7d30c51b22b2489294b136180c1c72
developer9019 <tnsucompany@naver.com>
2025-01-16 18:48:29 +0900

    Fix: Kiosk.java 파일 수정

--- src/com/example/level4/Kiosk.java
+++ src/com/example/level4/Kiosk.java
@@ -1,9 +1,33 @@
 package com.example.level4;
 
+import java.util.Scanner;
+
 public class Kiosk {
+private List<Menu> menuList;
+public Kiosk() {List<Menu> menulist) {this.menuList = menulist;}
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
 
-    start() {
+        while (true) {
 
+            // 1. 메뉴
+            MenuCommand menuCommand = handleMenuSelect(scanner);
+            if (menuCommand.getCommand().equals("exit")) {
+                break;
+            }
+            if (menuCommand.getCommand().equals("retry")) {
+                continue;
+            }
+            Menu selectedMenu = menuCommand.getSelectedMenu();
+
+            // 2. 아이템
+            MenuItemCommand menuItemCommand = handleMenuItemSelect(scanner, selectedMenu);
+            if (menuItemCommand.getCommand().equals("retry")) {
+                continue;
+            }
+            MenuItem selectedMenuItem = menuItemCommand.getSelectedMenuItem();
+
+        }
+        scanner.close();
     }
-
 }
